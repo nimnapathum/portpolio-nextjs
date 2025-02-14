23787a092d57800cb7310fcb6d53379774bdad28
nimnapathum <pathumeshan123@gmail.com>
2025-02-14 09:32:37 +0530

    changes on header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <header className="py-8 xl:py-12 text-white">
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/">
-                    <h1 className="text-4xl font-semibold">Nimna<span className="text-accent">.</span></h1>
+                    <h1 className="text-4xl font-semibold">Nimna<span className="text-accent">Pathum</span></h1>
                 </Link>
 
                 <div className="hidden xl:flex items-center gap-8">
